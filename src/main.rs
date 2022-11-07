37ec87ea21fad51aa58ccebd5af7c9397ec26b05
load28 <tjalsdud89@naver.com>
2022-11-07 22:26:04 +0900

    add data_type

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,45 @@ use rand::{Rng, thread_rng};
 
 fn main() {
     // run_guessing_game();
-    run_variable();
+    // run_variable();
+    run_data_type();
+}
+
+fn run_data_type() {
+    // 러스트는 기본적으로 모두 타입을 가져야 한다, parse 함수는 숫자 타입을 다 가질 수 있으므로 타입을 지정해주어야 한다
+    let guess: u32 = "42".parse().expect("error");
+    println!("guess - {}", guess);
+
+    // 정수, 부동소수, boolean, 문자 타입을 스칼라 타입이라 한다
+    let scalar: u32 = 10;
+    // isize, usize는 cpu의 아키텍처에 따라 비트가 결정됨 (32bit, 64bit)
+    let i_size: isize = -100;
+    let u_size: usize = 100;
+    println!("{}, {}, {}", scalar, i_size, u_size);
 }
 
 fn run_variable() {
+    // 상수는 타입이 반드시 필요하다
+    const MAX_POINT: u32 = 10000;
+
+    // mut는 가변성을 나타냄
     let mut x = 5;
     println!("x - {}", x);
     x = 10;
     println!("x - {}", x);
+
+    let y = 10;
+    // shadowing이 가능하다
+    let y = y + 20;
+    // 이전의 값을 계속해서 참조하여 shadowing 할 수 있다
+    let y = y + 20;
+    println!("y - {}", y);
+
+    let z = "    ";
+    // shadowing은 같은 변수에 다른 유형의 데이터를 넣을 수 있다, 이와 반대로 mut은 형변환을 허용하지 않는다
+    let z = z.len();
+    println!("z - {}", z);
 }
-
-
 
 fn run_guessing_game() {
     println!("Guess the number!");
